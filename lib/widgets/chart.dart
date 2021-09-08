c07385c09233781e33ed8051b5d6684828c319ea
akshaymunot30 <akshaymunot30@gmail.com>
2021-09-09 02:23:54 +0530

    Added space between the chart bars.
    - Added the 'FittedBox()' to push the amount to next line if amount's
    size increases, so as to make the bar fit in available space.
    - Also used 'Flexible' widget to make sure that the value of amount
    shrinks if its size increases.
    - Still getting the 'google_fonts' error saying 'unable to load
    Quicksand - Light from url' from last commit.

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -24,8 +24,8 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E().format(weekDay));
-      print(totalSum);
+      // print(DateFormat.E().format(weekDay));
+      // print(totalSum);
 
       return {
         'day': DateFormat.E().format(weekDay).substring(0, 1),
@@ -45,14 +45,23 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          return ChartBar(
-            (data['day'] as String),
-            (data['amount'] as double),
-            maxSpending == 0.0 ? 0.0 : (data['amount'] as double) / maxSpending,
-          );
-        }).toList(),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                (data['day'] as String),
+                (data['amount'] as double),
+                maxSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / maxSpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
