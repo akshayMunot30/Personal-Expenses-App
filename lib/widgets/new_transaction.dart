965334127f1ca68bac1cacdcbdb715bdef346d81
akshaymunot30 <akshaymunot30@gmail.com>
2021-10-31 00:29:06 +0530

    Added new feature & tried new concept practically
    - Learnt about the Widget Life Cycle, and tried to implement it
    practically.
    - Used initState() and dispose()
    - Added a new feature of changing color of the circle avatar
    and used the key functionality to link element and widget.

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,16 +7,43 @@ import '../widgets/adaptive_textbutton.dart';
 class NewTransaction extends StatefulWidget {
   final Function addTx;
 
-  NewTransaction(this.addTx);
+  NewTransaction(this.addTx) {
+    print("Constructor NewTransaction Widget");
+  }
 
   @override
-  _NewTransactionState createState() => _NewTransactionState();
+  _NewTransactionState createState() {
+    print("CreateState NewTransaction Widget");
+    return _NewTransactionState();
+  }
 }
 
 class _NewTransactionState extends State<NewTransaction> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+
+  _NewTransactionState() {
+    print("Constructor NewTransaction Widget");
+  }
+
+  @override
+  void initState() {
+    print("initState()");
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant NewTransaction oldWidget) {
+    print("didUpdateWidget()");
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    print("dispose()");
+    super.dispose();
+  }
 
   void _submitData() {
     if (_amountController.text.isEmpty) {
@@ -95,7 +122,8 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked date: ${DateFormat.yMd().format(_selectedDate!)}',
                       ),
                     ),
-                    AdaptiveTextButton(text: "Choose Date", handler: _presentDatePicker),
+                    AdaptiveTextButton(
+                        text: "Choose Date", handler: _presentDatePicker),
                   ],
                 ),
               ),
--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,10 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key? key,
     required this.transaction,
@@ -13,6 +15,26 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction transaction;
   final Function deleteTx;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bg = Colors.white;
+
+  @override
+  void initState() {
+    const availableColor = [
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.purple
+    ];
+
+    _bg = availableColor[Random().nextInt(4)];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,33 +46,33 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bg,
           radius: 30,
           child: Padding(
             padding: EdgeInsets.all(7),
-            child: FittedBox(
-                child: Text('\$${transaction.amount}')),
+            child: FittedBox(child: Text('\$${widget.transaction.amount}')),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: GoogleFonts.quicksand(
               textStyle: Theme.of(context).textTheme.headline6),
         ),
         subtitle: Text(
-          DateFormat.yMMMMd().format(transaction.date),
+          DateFormat.yMMMMd().format(widget.transaction.date),
         ),
         trailing: MediaQuery.of(context).size.width > 460
             ? TextButton.icon(
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
                 icon: const Icon(Icons.delete),
                 label: const Text("Delete"),
-                style: TextButton.styleFrom(
-                    primary: Theme.of(context).errorColor),
+                style:
+                    TextButton.styleFrom(primary: Theme.of(context).errorColor),
               )
             : IconButton(
                 icon: const Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
               ),
       ),
     );
